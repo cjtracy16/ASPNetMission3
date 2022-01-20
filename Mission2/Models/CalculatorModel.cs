6fa5b9445df7fcf3618db1bbce7e05376cc171d9
cjtracy <connertracy@comcast.net>
2022-01-19 20:53:37 -0700

    Mission3InitialPush

---
+++ Mission2/Models/CalculatorModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2.Models
+{
+    public class CalculatorModel
+    {
+        [Required]
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public double Assignment { get; set; }
+        [Required]
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public double Project { get; set; }
+        [Required]
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public double Quiz { get; set; }
+        [Required]
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public double Exam { get; set; }
+        [Required]
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public double Intex { get; set; }
+    }
+}
---
+++ Mission2/wwwroot/js/calculator.js
@@ -0,0 +1,60 @@
+﻿$("#calculate").click(function () {
+
+    const displayOutput = document.querySelector('#display');
+
+    let assignments = parseFloat($("#Assignments").val());
+    let projects = parseFloat($("#Projects").val());
+    let quizzes = parseFloat($("#Quizzes").val());
+    let exams = parseFloat($("#Exams").val());
+    let intex = parseFloat($("#INTEX").val());
+    let finalscore = ((assignments * .55) + (projects * .05) + (quizzes * .10) + (exams * .20) + (intex * .10)).toFixed(2);
+    let finalgrade = "N";
+
+    /*alert(`Your final score is ${finalscore}!`);*/
+
+    //if (isNaN(assignments) || isNaN(projects) || isNaN(quizzes) || isNaN(exams) || isNaN(intex)) {
+    //    displayOutput.textContent = `Please Input a Valid Integer in all Fields`;
+    //}
+
+    //else {
+    if (finalscore >= 94) {
+        finalgrade = "A";
+    }
+    else if (finalscore >= 90) {
+        finalgrade = "A-";
+    }
+    else if (finalscore >= 87) {
+        finalgrade = "B+";
+    }
+    else if (finalscore >= 84) {
+        finalgrade = "B";
+    }
+    else if (finalscore >= 80) {
+        finalgrade = "B-";
+    }
+    else if (finalscore >= 77) {
+        finalgrade = "C+";
+    }
+    else if (finalscore >= 74) {
+        finalgrade = "C";
+    }
+    else if (finalscore >= 70) {
+        finalgrade = "C-";
+    }
+    else if (finalscore >= 67) {
+        finalgrade = "D+";
+    }
+    else if (finalscore >= 64) {
+        finalgrade = "D";
+    }
+    else if (finalscore >= 60) {
+        finalgrade = "D-";
+    }
+    else {
+        finalgrade = "E";
+    }
+
+    displayOutput.textContent = `Final Score: ${finalscore} \n Final Grade: ${finalgrade}`
+    //}
+    
+})
